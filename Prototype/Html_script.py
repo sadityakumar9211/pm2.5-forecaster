d19b5a76fda87cf552ccc7cf6948988f96f08048
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-10-14 07:14:28 +0530

    squashed bugs. Now workly correctly

--- Prototype/Html_script.py
+++ Prototype/Html_script.py
@@ -12,45 +12,26 @@ import sys
 def retrieve_html():
     """retrieves the meteorological data from July 2018 to September 2022"""
     for year in range(2018, 2023):
-        if year == 2018:
-            for month in range(7,13):
-                if year == 2022 and month == 10:
-                    break
+        start_month = 7 if year == 2018 else 1
+        end_month = 10 if year == 2022 else 13
+        for month in range(start_month, end_month):
+            if year == 2022 and month == 10:
+                break
 
-                if month <= 9:
-                    url = "https://en.tutiempo.net/climate/{}-0{}/ws-428070.html".format(month, year)
-                else:
-                    url = "https://en.tutiempo.net/climate/{}-{}/ws-428070.html".format(month, year)
+            if month <= 9:
+                url = "https://en.tutiempo.net/climate/{}-0{}/ws-428070.html".format(month, year)
+            else:
+                url = "https://en.tutiempo.net/climate/{}-{}/ws-428070.html".format(month, year)
 
-                # retrieves the entire html from the specified url
-                texts = requests.get(url)
-                text_utf = texts.text.encode('utf-8')
+            # retrieves the entire html from the specified url
+            texts = requests.get(url)
+            text_utf = texts.text.encode('utf-8')
 
-                if os.path.exists("Data/Html_Data/{}".format(year)) is False:
-                    os.makedirs("Data/Html_Data/{}".format(year))
+            if os.path.exists("Data/Html_Data/{}".format(year)) is False:
+                os.makedirs("Data/Html_Data/{}".format(year))
 
-                with open("Data/Html_Data/{}/{}.html".format(year, month), "wb") as output:
-                    output.write(text_utf)
-
-        else:
-            for month in range(1, 13):
-                if year == 2022 and month == 10:
-                    break
-
-                if month <= 9:
-                    url = "https://en.tutiempo.net/climate/{}-0{}/ws-428070.html".format(month, year)
-                else:
-                    url = "https://en.tutiempo.net/climate/{}-{}/ws-428070.html".format(month, year)
-
-                # retrieves the entire html from the specified url
-                texts = requests.get(url)
-                text_utf = texts.text.encode('utf-8')
-
-                if os.path.exists("Data/Html_Data/{}".format(year)) is False:
-                    os.makedirs("Data/Html_Data/{}".format(year))
-
-                with open("Data/Html_Data/{}/{}.html".format(year, month), "wb") as output:
-                    output.write(text_utf)
+            with open("Data/Html_Data/{}/{}.html".format(year, month), "wb") as output:
+                output.write(text_utf)
 
         sys.stdout.flush()
 
@@ -59,5 +40,4 @@ if __name__ == "__main__":
     start_time = datetime.now()
     retrieve_html()
     end_time = datetime.now()
-
     print("Time taken: {} seconds".format((end_time - start_time).total_seconds()))
